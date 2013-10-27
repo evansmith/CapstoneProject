8b01e4fa3c0490bb06c3110b8238826781845ebb
Evan Smith <smith.evanj@gmail.com>
2013-10-27 01:35:47 -0400

    caught on a bug, made some progress

--- CapstoneProject/CapstoneProject/Scripts/CapstoneApp/App.js
+++ CapstoneProject/CapstoneProject/Scripts/CapstoneApp/App.js
@@ -8,16 +8,53 @@
 
 
 var MainCtrl = function ($scope, $location) {
-    $scope.sound1 = new buzz.sound('http://localhost:50990/Sounds/ctu-24-ringtone.mp3', {
-        preload: true,
-        autoplay: false,
-    });
-    $scope.sound2 = new buzz.sound("\Users\smith_000\Downloads\16300_ltibbits_snare_2_high_vol");
-    $scope.sounds = [
-        { sound: $scope.sound1, name: "Sound1" },
-        { sound: $scope.sound2, name: "Sound2" }
-    ];
 
+    $scope.init = function () {
+        $scope.sound1 = new buzz.sound('http://localhost:50990/Sounds/ctu-24-ringtone.mp3', {
+            preload: 'metadata'
+        });
+        $scope.sound2 = new buzz.sound('http://localhost:50990/Sounds/45666_pjcohen_Zildjian_A_Custom_Hi_Hat_Cymbals_Loose_Hit.WAV', {
+            preload: 'metadata'
+        });
+
+        $scope.sound3 = new buzz.sound('http://localhost:50990/Sounds/shopping cart.mp3', {
+            preload: 'metadata'
+        });
+
+        $scope.sounds = [
+           { sound: $scope.sound1, name: "24 Beep", description: "Sample from the television show 24" },
+           { sound: $scope.sound2, name: "Cymbal-Hi Hat", description: "A Sample of a Zildjian Hi Hat Cymbal" },
+           { sound: $scope.sound3, name: "Long Test", description: "Testing length" }
+        ];
+
+        //$scope.showIt = function (duration) {
+        //    console.log(duration);
+        //    return duration;
+        //}
+
+        $scope.getSoundDuration = function (sound) {
+            var duration = "";
+            sound.bind("loadedmetadata", function (duration) {
+                duration = buzz.toTimer(sound.getDuration());
+                console.log(duration);
+                return duration;
+            });  
+        }
+
+        
+          
+
+       
+
+
+        
+    }
+
+    $scope.init();
+    
+    
+   
+    //write a method here that creates a sound more efficiently
 
     $scope.play = function (sound) {
         console.log("im being clicked!", sound);
